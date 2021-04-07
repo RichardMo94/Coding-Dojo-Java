376cee44ed64d3fe53b208ab3b53f70dd3d18e6b
Richard Morales <richy.m94@hotmail.com>
2021-04-07 14:17:33 -0700

    Java Bank Account

--- JavaOOPAdvanced/BankAccount/src/com/codingdojo/bankaccount/BankAccountTest.java
+++ JavaOOPAdvanced/BankAccount/src/com/codingdojo/bankaccount/BankAccountTest.java
@@ -3,17 +3,17 @@ package com.codingdojo.bankaccount;
 public class BankAccountTest {
 	public static void main(String[] args) {
 		
-		BankAccount user1 = new BankAccount();
-		BankAccount user2 = new BankAccount();
-		BankAccount user3 = new BankAccount();
-		user1.accountDeposit("Savings", 60.00);
-		user2.accountDeposit("checking", 500.00);
-		user3.accountDeposit("savings", 2000.50);
-		user1.accountWithdrawal("Checking", 70.00);
-		user2.accountWithdrawal("checking", 200.00);
-		user3.accountWithdrawal("savings", 1000.00);
-		user2.myBalance();
-		user1.myBalance();
-		user3.myBalance();
-		}
+		BankAccount user_1 = new BankAccount();
+		BankAccount user_2 = new BankAccount();
+		BankAccount user_3 = new BankAccount();
+		user_1.accountDeposit("Savings", 60.00);
+		user_2.accountDeposit("checking", 500.00);
+		user_3.accountDeposit("savings", 2000.50);
+		user_1.accountWithdrawal("Checking", 70.00);
+		user_2.accountWithdrawal("checking", 200.00);
+		user_3.accountWithdrawal("savings", 1000.00);
+		user_2.myBalance();
+		user_1.myBalance();
+		user_3.myBalance();
+	}
 }
